24e5f2b36a2a7317883bdbfbd147d793b4783cb5
Magdalena <magdalena.traber@gmx.net>
2021-03-04 11:10:22 +0100

    Initial Commit

---
+++ src/HelloWorld/HelloWorld.java
@@ -0,0 +1,19 @@
+package HelloWorld;
+
+// Kommentare
+/*
+1. Klassenname muss Dateiname entsprechen
+2. Ändern > Refactor (SHIFT + F6)
+
+ */
+public class HelloWorld {
+    //
+    public static void main(String[] args) {
+        // Startpunkt, für unsere Programme
+        System.out.println("Hello World on day 4321");
+        // Liebes System
+        // out ausgeben
+        // abkürzung "sout"
+        // TODO: Some more code
+    }
+}
